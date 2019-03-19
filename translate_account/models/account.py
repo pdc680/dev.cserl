f753f965795cdcf46852236f094828a8eb1f99ac
pdc680 <40815097+pdc680@users.noreply.github.com>
2019-03-20 02:22:31 +0800

    Update account.py

--- translate_account/models/account.py
+++ translate_account/models/account.py
@@ -8,3 +8,10 @@ class AccountAccount(models.Model):
     _inherit = 'account.account'
 
     name = fields.Char(required=True, index=True, translate=True)
+    
+class AccountTax(models.Model):
+    _inherit = 'account.tax'
+
+    name = fields.Char(translate=True)
+    description = fields.Char(translate=True)
+    
