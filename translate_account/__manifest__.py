c4edc72e82d469a32b8d28077dc1e280855b1923
pdc680 <40815097+pdc680@users.noreply.github.com>
2019-03-20 01:27:25 +0800

    Add files via upload

---
+++ translate_account/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Make fields translatable demo',
+    'version': '1',
+    'description': """
+    """,
+    'author': 'Odoo Limited',
+    'depends': [
+        'account'
+    ],
+    'installable': False,
+    'auto_install': False,
+    'application': True,
+}
