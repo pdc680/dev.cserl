9803ab352756590438f45b9fe2d7f5abc0c4e012
pdc680 <40815097+pdc680@users.noreply.github.com>
2019-03-20 09:43:46 +0800

    Add files via upload

---
+++ expense_enhance/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Expense enhance',
+    'version': '1',
+    'description': """
+    """,
+    'author': 'PDC',
+    'depends': [
+        'expense'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
