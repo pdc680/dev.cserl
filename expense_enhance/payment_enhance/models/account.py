4213d718c7f0bdac4a9ba47bdb33d54f23755066
pdc680 <40815097+pdc680@users.noreply.github.com>
2019-03-21 14:14:31 +0800

    Update account.py

--- expense_enhance/payment_enhance/models/account.py
+++ expense_enhance/payment_enhance/models/account.py
@@ -7,4 +7,4 @@ from odoo import fields, models
 class HrExpenseSheet(models.Model):
     _inherit = "account.payment"
 
-    destination_account_id = fields.Many2one('account.account', domain=[('company_id', '=', 'company_id'), ('internal_type', '=', 'payable')], readonly=False)
+    destination_account_id = fields.Many2one('account.account', compute='_compute_destination_account_id', domain=[('company_id', '=', 'company_id'), ('internal_type', '=', 'payable')], readonly=False)
