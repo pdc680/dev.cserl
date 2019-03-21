3789f1feeece270cb5f514e3458b8ed64bc0ef17
pdc680 <40815097+pdc680@users.noreply.github.com>
2019-03-21 09:46:09 +0800

    Add files via upload

---
+++ expense_enhance/payment_enhance/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Payment enhance',
+    'version': '1',
+    'description': """
+    """,
+    'author': 'PDC',
+    'depends': [
+        'account'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ expense_enhance/payment_enhance/models/account.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+
+class HrExpenseSheet(models.Model):
+    _inherit = "account.payment"
+
+    destination_account_id = fields.many2one(readonly=False)
